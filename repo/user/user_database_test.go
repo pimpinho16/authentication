392f59ea286d85764b1cdc2c55ef888d23b7b251
aandrade <alexander.andrade@lifemiles.com>
2021-02-03 16:14:02 -0600

    agregando prueba para tabla de users

--- repo/user/user_database_test.go
+++ repo/user/user_database_test.go
@@ -1,1 +1,55 @@
 package user
+
+import (
+	"database/sql"
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"reflect"
+	"testing"
+)
+
+func TestNewUserDataBase(t *testing.T) {
+	type args struct{
+		db *gorm.DB
+	}
+
+	db,_ := mockDb()
+
+
+	testArgs := args{
+		db: db,
+	}
+	tests := []struct{
+		name string
+		args args
+		want *UserDatabase
+	}{
+		{
+			name : "NewUserDataBaseTest",
+			args: testArgs,
+			want: &UserDatabase{db},
+		},
+	}
+
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if got := NewUserDataBase(tt.args.db);  !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewUserDataBase() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func mockDb() (*gorm.DB, sqlmock.Sqlmock) {
+	var (
+		db     *sql.DB
+		gormDb *gorm.DB
+		mock   sqlmock.Sqlmock
+	)
+	db, mock, _ = sqlmock.New()
+	gormDb, _ = gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	return gormDb, mock
+}
